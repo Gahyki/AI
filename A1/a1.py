3ec7f59e111c587c0db23cfb80dd78a4371d1244
alireza ziarizi <61208934+Ziarizi@users.noreply.github.com>
2021-02-13 21:37:48 -0500

    split

---
+++ A1/a1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
+split_point = int(0.80*len(all_docs))
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
