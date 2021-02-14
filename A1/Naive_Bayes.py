89f34076887bd76e071dd13bd06b382ad4f531d3
Jason Kim <jason.k99@hotmail.com>
2021-02-13 22:18:41 -0500

    label and docs split

--- A1/Naive_Bayes.py
+++ A1/Naive_Bayes.py
@@ -1,17 +1,21 @@
 import numpy as np
 
 def read_documents(file):
+    labels = []
+    docs = []
     txt = open(file, encoding='UTF=8')
     for i in txt.readlines():
-        print(i)
+        labels.append(i.lower().split()[:2])
+        docs.append(i.lower().split()[2:])
+    return docs, labels
 
-read_documents('all_sentiment_shuffled.txt')
 
-# all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
-# split_point = int(0.80*len(all_docs))
-# train_docs = all_docs[:split_point]
-# train_labels = all_labels[:split_point]
-# #the rest of 20%
-# eval_docs = all_docs[split_point:]
-# eval_labels = all_labels[split_point:]
+all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
+split_point = int(0.80*len(all_docs))
+train_docs = all_docs[:split_point]
+train_labels = all_labels[:split_point]
+#the rest of 20%
+eval_docs = all_docs[split_point:]
+eval_labels = all_labels[split_point:]
+print(train_labels)
 
