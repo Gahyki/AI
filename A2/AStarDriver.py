0527b279da2e50be8b0f3ca7429b1ea3b1a23ae7
Jason Kim <jason.k99@hotmail.com>
2021-03-30 21:09:19 -0400

    Main bulk of A* completed

--- A2/AStarDriver.py
+++ A2/AStarDriver.py
@@ -1,6 +1,27 @@
 from AStar import *
+# import multiprocessing
+# import time
+# from threading import Timer, Thread
+#
+#
+# def check_timeout(status):
+#     thread = Thread(target=astar, name="A*", args=(puzzle))
+#     t = Timer(60, astar)
+#     t.start()
+#     thread.start()
 
 
-start = [[6, 1, 2], [7, 8, 3], [5, 4, 9]]
-end = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# start = ((6, 1, 2), (7, 8, 3), (5, 4, 9))
+start = ((5, 1, 2, 4),(9, 6, 3, 7),(13, 10, 16, 8),(14, 15, 11, 12))
 
+puzzle = Puzzle(start)
+solution, search = astar(puzzle)
+
+print("-------------------------- Solution -----------------------------")
+for p in solution:
+    print(p)
+print(len(solution))
+print("-------------------------- Search -----------------------------")
+for s in search:
+    print(s)
+print(len(search))
