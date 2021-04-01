9b9bad7029607104dc8e09b1cbb10503770a0b16
alireza ziarizi <61208934+Ziarizi@users.noreply.github.com>
2021-03-31 22:26:18 -0400

    back to normal

--- A2/AStar.py
+++ A2/AStar.py
@@ -1,49 +1,170 @@
-import math
 import time
-import numpy
+import heapq
+import sys
+class Puzzle:
+    def __init__(self, matrix):
+        # current state of puzzle, xy location of each value
+        if type(matrix) is list:
+            self.puzzle = matrix
+        else:
+            self.puzzle = self.__matrixtopuzzle(matrix)
+        self.size = len(matrix)
+        self.heuristic = self.heuristics2()
 
+    def __matrixtopuzzle(self, matrix):
+        # puzzle list has each element of matrix in a single dimension form
+        length = len(matrix)
+        puzzle = []
+        for i in range(length):
+            for j in range(length):
+                puzzle.append(matrix[i][j])
+        return puzzle
 
-def createhash(arr):
-    hashtable = {}
-    size = len(arr)
-    for i in range(size):
-        for j in range(size):
-            hashtable[arr[i][j]] = (i, j)
-    return hashtable, size
+    def neighbors(self):
+        # All nodes connected to this node
+        unique = {}
+        result = []
+        # Combination x and y (numbers that could be applied)
+        cx = [1, -1, 0, 0]
+        cy = [0, 0, 1, -1]
+        # Generate all possibilities
+        for i, v in enumerate(self.puzzle):
+            # Get x and y coordinates of the chosen value
+            oldx = i // self.size
+            oldy = i % self.size
+            for px, py in zip(cx, cy):
+                # Get new x and y coordinates of the generated possibility
+                newx = oldx + px
+                newy = oldy + py
+                if 0 <= newx < self.size and 0 <= newy < self.size:
+                    temppuzzle = list(self.puzzle)
+                    newindex = newx * self.size + newy
+                    temppuzzle[oldx * self.size + oldy] = temppuzzle[newindex]
+                    temppuzzle[newindex] = v
+                    if str(temppuzzle) not in unique.keys():
+                        unique[str(temppuzzle)] = v
+                        obj = Puzzle(temppuzzle)
+                        obj.size = self.size
+                        result.append(obj)
+        return result
 
+    def heuristics1(self):
+        hsum = 0
+        # Regular Hamming distance
+        for i, v in enumerate(self.puzzle):
+            if v != i + 1:
+                hsum += 1
+        return hsum
 
-def creatematrix(hashtable, size):
-    matrix = [[0 for _ in range(size)] for _ in range(size)]
-    for v, i, j in hashtable.items():
-        matrix[i][j] = v
-    return matrix
-
-
-def heuristics1(current, goal, values, size):
-    # both matrices
-    hsum = 0
-    for v in values:
-        # Regular Manhattan Distance
-        hsum += abs(goal[v][0] - current[v][0]) + abs(goal[v][1] - current[v][1])
-    return hsum
-
-
-def heuristics2(current, goal, values, size):
-    # both matrices
-    hsum = 0
-    for v in values:
+    def heuristics2(self):
+        hsum = 0
         # Manhattan Distance For Matrix
-        hsum += min(abs(goal[v][0] - current[v][0]), size - 1 - abs(goal[v][0] - current[v][0])) \
-                + min(abs(goal[v][1] - current[v][1]), size - 1 - abs(goal[v][1] - current[v][1]))
-    return hsum
+        for i, v in enumerate(self.puzzle):
+            # Get starting position
+            sx = i // self.size
+            sy = i % self.size
+            # Get ending position
+            ex = (v - 1) // self.size
+            ey = (v - 1) % self.size
+            # hsum += abs(ex - sx) + abs(ey - sy)
+            hsum += min(abs(ex - sx), self.size - 1 - abs(ex - sx)) \
+                    + min(abs(ey - sy), self.size - 1 - abs(ey - sy))
+        return hsum
 
+    def solved(self):
+        return self.heuristic == 0
 
-def gdist(startx: int, starty: int, endx: int, endy: int):
-    a = endx - startx
-    b = endy - starty
-    return a + b
+    def __str__(self):
+        # matrix output in a string form
+        matrix = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        # index/value
+        for i, v in enumerate(self.puzzle):
+            x = i // self.size
+            y = i % self.size
+            matrix[x][y] = v
+        output = "\n--- Puzzle Output ---\n"
+        for row in matrix:
+            output += str(row) + "\n"
+        output += "--- Puzzle Output ---\n"
+        return str(output)
 
+    def __lt__(self, other):
+        return self.heuristic < other.heuristic
 
+        def __le__(self, other):
+            return self.heuristic <= other.heuristic
 
-# Step 1 - Create hashtables to make searching faster for current and goal
-# Step 2 - Create heuristic based on hashtable
+    class OpenList:
+        # Min Priority Queue
+        def __init__(self):
+            # List of all potential elements
+            self.elements = []
+
+        def put(self, heuristic_value, potential):
+            # Pushed into queue using heap
+            heapq.heappush(self.elements, (heuristic_value, potential))
+
+        def get(self):
+            # returns only the potential element
+            # the heuristic value is not returned
+            return heapq.heappop(self.elements)
+
+        def isempty(self):
+            return not self.elements
+
+        def checkinside(self, option):
+            # Method to check if neighbor is inside the MinPQ
+            for _, puzzle in self.elements:
+                if puzzle == option:
+                    return True
+            return False
+
+    def astar(puzzle):
+        # List of different possibilities
+        openlist = OpenList()
+        openlist.put(0, puzzle)
+
+        # Solution path
+        closelist = []
+
+        # Dictionary containing state of matrix in string form to find avoid repetition of actions
+        uniquecloselist = {}
+
+        # Cost of each possibility
+        costlist = {}
+
+        # Initializing cost list
+        costlist[str(puzzle.puzzle)] = 0
+
+        # List of considered possibilities
+        searchlist = []
+
+        # Main loop
+        starttime = time.time()
+        while not openlist.isempty():
+            # Current node to explore
+            current = openlist.get()
+
+            # Adding to closelists
+            closelist.append(current[1])
+            uniquecloselist[str(current[1].puzzle)] = None
+
+            if (time.time() - starttime) > 60:
+                closelist = ["no solution"]
+                searchlist = ["no solution"]
+                print("It took too long to solve...")
+                return closelist, searchlist
+
+            # Compare with the goal
+            if current[1].solved():
+                break
+            else:
+                # Getting each neighbor and its heuristic value
+                for option in current[1].neighbors():
+                    new_cost = costlist[str(current[1].puzzle)] + 1
+                    if str(option.puzzle) not in uniquecloselist.keys() or new_cost < costlist[str(option.puzzle)]:
+                        fscore = new_cost + option.heuristic
+                        costlist[str(option.puzzle)] = new_cost
+                        openlist.put(fscore, option)
+                        searchlist.append(option)
+        return closelist, searchlist, max(costlist.values()), (time.time() - starttime)
--- A2/DepthFirstDriver.py
+++ A2/DepthFirstDriver.py
@@ -3,11 +3,11 @@ import time
 
 f = open("puzzles.txt", "r")
 results = open("outputDepth.txt",'w')
-counter = 0
 
 for line in f:
     start = eval(line)
     solution = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
     puzzle = Puzzle(start)
     puzzle.depthFirstSearch(puzzle,solution)
+
 
